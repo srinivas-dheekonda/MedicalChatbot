643737c3cb30c6fae63acf5eca56ff5e6d22b401
srinivas-dheekonda <vasuvasu.srinu1996@gmail.com>
2026-03-09 12:54:32 +0530

    installed requirements.txt

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p reseach
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reseach/trails.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully"
+
