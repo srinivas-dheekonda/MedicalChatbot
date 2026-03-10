f23afcda43773d1a1618979501a7edeaeeee57cd
srinivas-dheekonda <vasuvasu.srinu1996@gmail.com>
2026-03-10 10:48:44 +0530

    Experiment done and tested

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MedicalChatbot",
+    version = "0.1.0",
+    description = "A chatbot for medical questions",
+    author = "Srinivas",
+    author_email = "vasuvasu.srinu1996@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
